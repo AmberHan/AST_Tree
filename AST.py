dc8f208b2825370ea3f18bc966d33ce1393e05a4
AmberHan <1198391037@qq.com>
2020-11-26 17:04:11 +0800

    广度-深度优先遍历构建AST树

--- AST.py
+++ AST.py
@@ -5,90 +5,83 @@
 # !@File   : AST.py.py
 import queue
 import json
-# result_dict结果真值表 key:真值表;value:[代价,node]
+# result_dict结果真值表 key:every_id;value:node
 result_dict = {}
+every_id = 1
+result_dict1 = {}
+every_id1 = 1
 
 
 # father_node:父节点;children_list子节点;my_type:元电路对象;unitary矩阵,child=0减枝
 class AstNode(object):
-    def __init__(self, father_node, temp):
+    def __init__(self, father_node, node_id, temp):
         self.father_node = father_node
+        self.id = node_id
         self.type = int(temp["type"])
         self.label = temp.get("label")
         self.typeLabel = temp["typeLabel"]
         self.pos = int(temp["pos"])
         self.length = int(temp["length"])
-        # self.children = children
-        # self.id = temp["id"]
-        # self.parent = None
+        self.children = []
 
-    # 添加父亲的子节点,目的为了减枝
-    def add_children(self, children):
-        self.children_list.append(children)
-
-
-# 广度优先,循环寻找最优解
-# type_all:基本元情况； node:父节点
-def com_circuit(plies_node, type_all):
-    while not plies_node.empty():
-        plies_node_list_size = plies_node.qsize()
-        # 广度优先，每层遍历
-        for i in range(plies_node_list_size):
-            node = plies_node.get()
-            if node.child == 0:  # 节点非最优
-                continue
-            for circuit_i in range(len(type_all)):
-                if circuit_i == node.my_type:  # 相同的会抵消,不考虑
-                    continue
-                else:
-                    # 优化后 自己算的矩阵
-                    circuit_new_unitary = np.dot(type_all[circuit_i][2], node.unitary)
-                    unitary_tuple_new = tuple(unitary_list(circuit_new_unitary))
-                    node_cost = node.cost + type_all[circuit_i][1]
-                    node_new = EveryNode(node, circuit_i, node_cost, circuit_new_unitary)
-                    node.add_children(node_new)
-                    if unitary_tuple_new not in result_dict:  # 新的解答
-                        print('目前解数目：{}'.format(len(result_dict)))
-                        plies_node.put(node_new)
-                        result_dict[unitary_tuple_new] = [node_cost, node_new]
-                    elif unitary_tuple_new in result_dict and result_dict[unitary_tuple_new][0] > node_cost:  # 更优解
-                        plies_node.put(node_new)
-                        node_get = result_dict[unitary_tuple_new][1]  # 找到非优解进行child=0,child_list置空操作
-                        node_get.clear_children()
-                        result_dict[unitary_tuple_new] = [node_cost, node_new]  # 更新真值字典
-                    else:
-                        node_new.clear_children()
-                        continue
-
+    def add_children(self, child):
+        self.children.append(child)
 
 # 初始化
 def init_truth():
     with open('AST.txt', 'r') as f:
         data = json.load(f)
-    print(data)
+    # print(data)
     re_dist = data['root']
-    NewNode = AstNode(None, re_dist)
-    print(1)
-    for i in data['root']['children'][0]['children']:
-        print(i, '\n')
-        # AstNode(i)
-        # print(i['type'])
-        # AstNode(i)
-    #     for j in i['children']:
-    #         print(j['type'])
-    # print(data['root'])
-    # circuit_unitary = np.identity(2 ** n)
-    # node_0 = EveryNode(None, None, 0, circuit_unitary)
-    # tree_queue = queue.Queue()
-    # tree_queue.put(node_0)
-    # com_circuit(tree_queue, type_all)
-    # print('(数组列表):[代价，node标号]')
-    # # print(result_dict)
-    # print('真值表可表达的情况数目:{}'.format(len(result_dict)))
+    root_node = AstNode(None, every_id, re_dist)
+    result_dict[every_id] = root_node
+    result_dict1[every_id1] = root_node
+    re_list = data['root']['children']
+    # 法一 广度优先
+    node_queue = queue.Queue()
+    node_queue.put(root_node)
+    list_queue = queue.Queue()
+    list_queue.put(re_list)
+    gd_root(node_queue, list_queue)
+    # 法二 深度优先
+    sd_root(root_node, re_list)
+
+
+# 深度优先
+def sd_root(father_node, re_list):
+    global every_id
+    for children in re_list:
+        every_id += 1
+        new_node = AstNode(father_node, every_id, children)
+        father_node.add_children(new_node)
+        result_dict[every_id] = new_node
+        re_list = children['children']
+        sd_root(new_node, re_list)
+
+
+# 广度优先
+def gd_root(node_queue, list_queue):
+    global every_id1
+    while not node_queue.empty():
+        node_queue_size = node_queue.qsize()
+        for i in range(node_queue_size):
+            father_node = node_queue.get()
+            re_list = list_queue.get()
+            for children in re_list:
+                every_id1 += 1
+                new_node = AstNode(father_node, every_id, children)
+                father_node.add_children(new_node)
+                result_dict1[every_id1] = new_node
+                node_queue.put(new_node)
+                list_queue.put(children['children'])
 
 
 def main():
     init_truth()
+    print("深度节点数目", every_id)
+    print("广度节点数目", every_id1)
+    # print(result_dict)
+    # print(result_dict1)
 
 
 if __name__ == '__main__':
